cae5d8bbcbbc1a3ea85e98d3632d6a0c6b223638
Student <example@example.com>
2019-09-27 11:20:12 -0700

    add welcome message

---
+++ Models/Bread.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+namespace PierresBakery.Models
+{
+    public class Bread
+    {
+        public int BreadPrice;
+    }
+
+    public class Program
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using PierresBakery.Models;
+
+namespace PierresBakery
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Pierre's Bakery!");
+            Console.WriteLine("Bread: $5 for one loaf");
+            Console.WriteLine("Pastries: $2 each");
+            
+
+        }
+    }
+}
