0f2b913fc96aed6917b61b725ee4d2088efdfa35
Student <example@example.com>
2019-09-27 18:30:21 -0700

    Add interactivity to program

--- Program.cs
+++ Program.cs
@@ -8,14 +8,21 @@ namespace Bakery
     {
         public static void Main()
         {
+            int breadQty, pastryQty, totalCost;
+            Cart yourCart = new Cart();
             Console.WriteLine("Welcome to Pierre's Bakery!");
             Console.WriteLine("Bread is $5 for one loaf and pastries are $2 each");
-            Console.WriteLine("Special #1- Buy two loaves and get the third free");
+            Console.WriteLine("Special #1- Buy two loaves and get the third free!");
             Console.WriteLine("Special #2- Buy three for $5!");
-            
-            
-
-
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Please enter how many loaves you'd like to purchase");
+            breadQty = int.Parse(Console.ReadLine());
+            yourCart.AddBread(breadQty);
+            Console.WriteLine("Please enter how many pastries you'd like to buy");
+            pastryQty = int.Parse(Console.ReadLine());
+            yourCart.AddPastries(pastryQty);
+            totalCost = cart.CalculateTotal();
+            Console.WriteLine("Your total is ${0}!", totalCost);
         }
     }
 }
